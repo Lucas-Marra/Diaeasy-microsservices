ef7216587b018ce14a141ac5564aa17e2f940a69
Lucas Marra Rebello <lucasmrebello@yahoo.com.br>
2022-09-18 22:46:12 -0300

    Criação do microsserviço paciente

---
+++ paciente/src/main/java/br/com/tcc/PacienteApplication.java
@@ -0,0 +1,11 @@
+package br.com.tcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacienteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PacienteApplication.class, args);
+    }
+}
